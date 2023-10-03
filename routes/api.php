5253ccce535d7086c4108c10db2accd57e470dd4
yoshimar-cm <yoshi.cm3@gmail.com>
2023-10-02 23:03:40 -0600

    implement JWT
    creacion de login y la proteccion de rutas con JWT

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,5 +22,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::resource('categories', CategoryController::class);
-Route::resource('notes', NoteController::class);
+Route::group([
+    'middleware' => 'auth:api'
+],function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('notes', NoteController::class);
+});
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'v1/auth'
+
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('me', [AuthController::class, 'me'])->name('me');
+});
+
+
